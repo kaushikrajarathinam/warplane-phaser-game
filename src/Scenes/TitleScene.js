f44124ac3c241cf6a7fdd527e6cabfeabac4bf6a
Kaushik Rajarathinam <kaushik@Kaushiks-MacBook-Pro.local>
2025-06-21 04:28:24 -0700

    done

--- src/Scenes/TitleScene.js
+++ src/Scenes/TitleScene.js
@@ -2,7 +2,7 @@ class TitleScene extends Phaser.Scene {
     constructor() { super('TitleScene'); }
 
     create() {
-        this.add.text(300, 250, "GALLERY SHOOTER", { fontSize: '32px' });
+        this.add.text(300, 250, "WarPlanes", { fontSize: '32px' });
         this.add.text(290, 300, "Press SPACE to Start", { fontSize: '16px' });
         this.input.keyboard.once('keydown-SPACE', () => {
             this.scene.start('GameScene');
